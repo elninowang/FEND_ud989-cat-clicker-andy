6749c7354e47e10737be271728e85b726f32c285
ElNino Wang <elninowang@gmail.com>
2017-11-25 22:29:45 +0800

    完成多了，可以随时切换猫咪

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
-var ViewModel = function() {
+let Cat = function(data) {
 	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Cat1');
-	this.imgSrc = ko.observable('images/cat_picture1.jpg');
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.nicknames = ko.observableArray(data.nicknames);
+	
+	this.title = ko.computed(() => {
+		let title;
+		let clicks = this.clickCount();
+		if (clicks < 10) {
+			title = 'Newborn';
+		} else if (clicks < 50) {
+			title = 'Infant';
+		} else if (clicks < 100) {
+			title = 'Child';
+		} else {
+			title = 'Ninja';
+		}
+		return title;
+	}, this);
+};
+
+let ViewModel = function() {
+	let self = this;
+
+	this.carList = ko.observableArray([]);
+	for (let i = 1; i <= 5; i ++) {
+		this.carList.push(new Cat({
+			name: `Cat${i}`,
+			imgSrc: `images/cat_picture${i}.jpg`,
+			nicknames: [`Tabtab${i}`,`T-Bone${i}`,`Mr. T${i}`],
+		}));
+	} 
+
+	this.currentCat = ko.observable(this.carList()[0]);
 
 	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
+		//this.clickCount(this.clickCount() + 1); 不建议这样写，因为不好理解
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};
+
+	this.setCurrentCat = function(cat) {
+		self.currentCat(cat)
 	};
 };
 
